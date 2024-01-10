3d35df0746eca52cc8c5e1ddd44859dbd0968cd6
aidenpratt <u1458057@umail.utah.edu>
2024-01-10 16:20:11 -0700

    updates to website

--- aiden.js
+++ aiden.js
@@ -2,6 +2,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     let myExtension = document.getElementById("myExtension");
     let myPort = document.getElementById("myPort");
+    let myStrava = document.getElementById("myStrava");
 
     function newTab(event) {
         event.preventDefault();
@@ -11,5 +12,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
     myExtension.addEventListener("click", newTab);
     myPort.addEventListener("click", newTab);
+    myStrava.addEventListener("click", newTab);
 
 });
